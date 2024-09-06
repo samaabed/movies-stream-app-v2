b94f93428904ba5386b5e6a637fb80cbc335958c
sama <samaabed69@gmail.com>
2024-09-06 18:01:21 +0300

    fix: changing file names for right sidebar component

--- src/components/Home/RightSidebar/index.jsx
+++ src/components/Home/RightSidebar/index.jsx
@@ -1,4 +1,4 @@
-import styles from "./RightSidebar.module.css";
+import styles from "./index.module.css";
 import { useContext } from "react";
 import ThemeContext from "../../../contexts/ThemeContext";
 
